cc3f12fad42cf8ffb80f05acfef553aaed0d2535
Shidkung <pitpi@DESKTOP-B29REMC>
2021-08-10 12:51:35 +0700

    float x, y ;  scanf_s("%f %f", &x, &y);  printf("%f\n%f\n%f\n%f",x + y,x - y, x * y, x/y);

--- Project6/Source.cpp
+++ Project6/Source.cpp
@@ -2,9 +2,6 @@
 int main() {
 	float x, y ;
 	scanf_s("%f %f", &x, &y);
-	printf("%f\n", x+y);
-	printf("%f\n", x-y);
-	printf("%f\n", x*y);
-	printf("%f\n", x/y);
+	printf("%f\n%f\n%f\n%f",x + y,x - y, x * y, x/y);
 	return 0;
 }
